337fb37c0206152435230c5bab1406744c42a536
Angelo <amarano@gmail.com>
2013-02-01 06:50:39 -0500

    Added some tests
    And I haven't run any of them yet. Busy busy busy.

--- AssessMe/tests.py
+++ AssessMe/tests.py
@@ -6,7 +6,8 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from AssessMe.models import *
+from datetime import date
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,3 +15,38 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+
+class AverageScoreTest(TestCase):
+    def test_best_average_score(self):
+
+        multiple_choice = AssessmentType()
+        multiple_choice.name = "Multiple Choice"
+        multiple_choice.description = "Multiple Choice"
+
+        short_answer = AssessmentType()
+        short_answer.name = "Short Answer"
+        short_answer.description = "Short Answer"
+
+        essay = AssessmentType()
+        essay.name = "Essay"
+        essay.description = "Essay"
+
+        types = [multiple_choice, short_answer, essay]
+        student = Student()
+        student.first_name = "angelo"
+        student.last_name = "marano"
+        student.birthdate = date.today()
+        student.foreign_id = "12345678"
+
+        for i, t in enumerate(types): #I'm going to make the first type the best score
+            for i in range(5):
+                a = Assessment()
+                a.type = t
+                score = AssessmentScore()
+                score.assessment = a
+                score.value = 100.0 - i # subtract the index from the score, lame, right?
+                score.student = student
+
+        self.assertEqual(types[0], student.most_effective_assessment_type())
+        self.assertNotEqual(types[1], student.most_effective_assessment_type())
