948eada509e72d29c929f9595eeed71ec8fee1fb
Angelo <amarano@gmail.com>
2013-03-01 19:45:00 -0500

    Starting work on views

--- AssessMe/views.py
+++ AssessMe/views.py
@@ -1,1 +1,8 @@
 # Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import FormView
+
+class CreateUserView(FormView):
+    template_name = 'registration/newuser.html'
+    form_class = UserCreationForm
+    success_url = 'about'
